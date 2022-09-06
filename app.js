658db2ef0ab39e0628401f42225962395838a2f5
clevercoderjoy <clevercoderjoy@gmail.com>
2022-09-06 21:56:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,42 +5,26 @@ const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
-    hideMessage();
-
-    if (billAmount.value > 0){
-
-        if (cashGiven.value >= billAmount.value){
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    message.style.display = "none";
+    if (billAmount.value > 0) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
+        } else {
+            showMessage("The cash provided should be at least equal to bill amount.");
         }
-
-        else{
-            showMessage("The cash provided should be at least be equal to the bill amount");
-        }
+    } else {
+        showMessage("Invalid bill amount!");
     }
-
-    else{
-        showMessage("Bill amount has to be greater than 0");
-    }
-
 });
 
 function calculateChange(amountToBeReturned) {
-    // iterating over the available notes
-    for (let i = 0; i < availableNotes.length; i++){
-        // no of notes to be returned for a particular note
+    for (var i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-        // remaining amount
-        amountToBeReturned %= availableNotes[i];
-        // rendering number of notes to be returned in the document
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
-}
-
-function hideMessage(){
-    message.style.display = "none";
 }
 
 function showMessage(msg) {
